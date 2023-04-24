51ce1e4a84b496e4828d2499324c35a259e43760
FabianAlvarez1 <93471911+FabianAlvarez1@users.noreply.github.com>
2023-04-24 15:43:54 -0600

    Cambios de interfaz, búsqueda y subida de archivos
    1. Se agregó un nuevo estilo de vista "tarjetas".
    2. Se reemplazaron todos los iconos de imagen por svg para reducir el tiempo de carga.
    3. Se agregó un filtro de búsqueda de archivos por contenido.
    4. Se agregó un spinner a modo de animación de carga en los procesos del sistema.
    5. Se implementó la subida de carpetas y archivos para procesar como: .zip y .jsnwm (Websencia).

--- fileman/js/fileman.js
+++ fileman/js/fileman.js
@@ -9,6 +9,7 @@ var fileman={
 	selectedFiles:[],
 	codeExts:null,
 	files:null,
+	
 	onClickCopy:function()
 	{
 		var clipboard={
@@ -34,10 +35,37 @@ var fileman={
 
 		this.setCookieData(clipboard);
 	},
+	onClickDelete:function(){
+		if (fileman.selectedFiles.length===0){
+			alert("No ha seleccionado ningún archivo");
+			return;
+		}
+		if(!confirm("¿Está seguro que desea eliminar los archivos seleccionados?"))
+			return;
+
+		var rq={};
+		for(const er of fileman.selectedFiles)
+		{
+			rq[er]={ action:"delete"};
+		}
+
+		fileman.invoque(rq,function(data)
+		{
+			let res = Object.keys(data);
+
+			for(var i=0; i<res.length; i++){
+				let r = res[i];
+
+				if(!data[r].done)
+					alert(r + " no se ha podido eliminar.\n" + data[r].message);
+			}
+		},
+		function(message){
+			alert("Ha ocurrido un error al realizar la operación\n\r"+message);
+		});
+	},
 	onClickPaste:function(){
 
-		// Validación de pegado
-		// ...
 		if(fileman.cookieData==null){
 			alert("No hay archivos en el portapapeles.");
 			return;
@@ -72,6 +100,7 @@ var fileman={
 	},
 	invoqueCopy:function(data, fromPath, callback_success, callback_fail)
 	{
+		fileman.waiting();
 		$.ajax({
 			type: "POST",
 			url: fromPath+"do.fso",
@@ -113,7 +142,6 @@ var fileman={
 			error: function(r){
 				alert("Ocurrió un error al invocar el servicio.\n\r"+r.statusText);
 			}
-
 		});
 	},
 	addSelectedFile:function(fn)
@@ -129,7 +157,19 @@ var fileman={
 		fileman.dropArea = document.querySelector(".fileman-viewfolder");
 		fileman.button = document.querySelector(".upload_file");
 		fileman.input = document.querySelector("#input_file");
-		
+		fileman.input_folder = document.querySelector("#input_folder");
+		fileman.input_zip = document.querySelector("#input_file_zip");
+		fileman.input_websencia = document.querySelector("#input_file_websencia");
+		fileman.up_file = document.querySelector("#up_file");
+		fileman.up_options = document.querySelector("#upload_options");
+
+		if(fileman.up_file)fileman.up_file.addEventListener('click', () => {
+			fileman.up_options.classList.toggle("upload_options_show");
+		});
+		if(fileman.up_options)fileman.up_options.childNodes.forEach(c => {
+			c.addEventListener('click', () => fileman.up_options.classList.remove("upload_options_show"));
+		});
+
 		//Encima del control
 		fileman.dropArea.addEventListener('dragover', e =>{
 			e.preventDefault();
@@ -142,33 +182,39 @@ var fileman={
 		});
 		//Cuando suelta el elemento en el control
 		try{
-			fileman.dropArea.addEventListener('drop', b => {
-				b.preventDefault();
+			fileman.dropArea.addEventListener('drop', async b => {
 				
 				fileman.input.files = b.dataTransfer.files;
+				const promise = fileHandler.getFiles(b);
+				var formData = new FormData();
+				
+				b.preventDefault();
+				b.stopPropagation();
+				
+				if(promise) await promise.then( v => {
+					
+					v.forEach( f => {
+						
+						let exist=(fileman.files && fileman.files.some(fe => fe.name==f.name));
+						
+						if (exist && !confirm(`El archivo ${f.name} ya existe en el directorio actual. ¿Desea reemplazarlo?`)){
+							//ignore
+						}else{
+							
+							var nf = new File([f.fileObject], f.name, {type:f.type});
+							formData.append(f.fullPath, nf, f.name);
+						}
+					});
+				});
 
-				if (b.dataTransfer.files.length < 1){
+				if (Array.from(formData.keys()).length < 1){
+
 					alert("Debe seleccionar al menos un archivo.");
 					fileman.dropArea.classList.remove('drop_active');
 					return;
 				}
-
-				let flag = false;
-				//console.log(b.dataTransfer.files[0]);return;
-				fileman.files.map(e=>{
-					if (e.name == b.dataTransfer.files[0].name){
-						if(!confirm("El archivo ya existe en el directorio. ¿Desea reemplazarlo?"))
-						{
-							fileman.dropArea.classList.remove('drop_active');
-							flag=true;
-						}
-					}
-				});
-	
-				if(flag) return;
-
 				
-				fileman.invoqueUpFiles(new FormData(document.getElementById("formData")), function(data)
+				fileman.invoqueUpFiles(formData, function(data)
 				{
 					let res = Object.keys(data);
 		
@@ -191,14 +237,16 @@ var fileman={
 		finally{
 			fileman.dropArea.classList.remove('drop_active');
 		}
-		
 
 		fileman.input.addEventListener('change', e => {
+			
 			let files = fileman.input.files;
+
 			if(files.length < 1){
 				alert("Debe seleccionar al menos un archivo.");
 				return;
 			}
+
 			fileman.invoqueUpFiles(new FormData(document.getElementById("formData")), function(data)
 			{
 				let res = Object.keys(data);
@@ -214,12 +262,122 @@ var fileman={
 				alert("Ha ocurrido un error al realizar la operación\n\r"+message);
 			});
 		});
+		fileman.input_zip.addEventListener('change', e => {
+			let files = fileman.input_zip.files;
+			
+			if(files.length < 1){
+				alert("Debe seleccionar al menos un archivo.");
+				return;
+			}
+			if(files.length > 1){
+				alert("Debe selecionar solo un archivo.");
+				return;
+			}
 
+			var fd = new FormData();
+			var f = files.item(0);
+
+			var nf = new File([f], f.name, {type:f.type})
+			fd.append(f.name+"*", nf, f.name);
+
+			fileman.invoqueUpFiles(fd, function(data)
+			{
+				let res = Object.keys(data);
+				let r = res[0];
+				if(!data[r].done) alert(r + " no se pudo subir.\n" + data[r].message);
+			},
+			function(message){
+				alert("Ha ocurrido un error al subir el archivo.\n\r"+message);
+			});
+		});
+		fileman.input_websencia.addEventListener('change', e => {
+			let files = fileman.input_websencia.files;
+			
+			if(files.length < 1){
+				alert("Debe seleccionar al menos un archivo.");
+				return;
+			}
+			if(files.length > 1){
+				alert("Debe selecionar solo un archivo.");
+				return;
+			}
+
+			var fd = new FormData();
+			var f = files.item(0);
+
+			var nf = new File([f], f.name, {type:f.type})
+			fd.append(f.name+(fileman.renderWebsencia?"*":""), nf, f.name);
+
+			fileman.invoqueUpFiles(fd, function(data)
+			{
+				let res = Object.keys(data);
+				let r = res[0];
+				if(!data[r].done) alert(r + " no se pudo subir.\n" + data[r].message);
+			},
+			function(message){
+				alert("Ha ocurrido un error al subir el archivo.\n\r"+message);
+			});
+		});
+		fileman.input_folder.addEventListener('change', e => {
+			
+			let files = fileman.input_folder.files;
+			
+			if(files.length < 1){
+				alert("Debe seleccionar al menos una carpeta que contenga archivos.");
+				return;
+			}
+
+			var fd = new FormData();
+
+			for(let i=0; i<files.length; i++)
+			{
+				var f = files.item(i);
+				var nf = new File([f], f.name, {type:f.type})
+				
+				fd.append(f.webkitRelativePath, nf, f.name);
+			}
+			
+			fileman.invoqueUpFiles(fd, function(data)
+			{
+				let res = Object.keys(data);
+	
+				for(var i=0; i<res.length; i++){
+					let r = res[i];
+	
+					if(!data[r].done)
+						alert(r + " no se pudo subir.\n" + data[r].message);
+				}
+			},
+			function(message){
+				alert("Ha ocurrido un error al realizar la operación\n\r"+message);
+			});
+		});
+	},
+
+	search:function(view){
+
+		let search = document.querySelector("#input_search");
+
+		if (search.value.trim()=='') return
 		
+		url=window.location.href;
+		let p = url.includes("?") ? '&' : '?';
+		url += p+"search="+search.value.trim()+"&view="+view;
+
+		window.location.href=url;
 	},
 
 	upl_file:function(){
-		fileman.input.click();	
+		fileman.input.click();
+	},
+	upl_file_zip:function(){
+		fileman.input_zip.click();
+	},
+	upl_file_websencia:function(){
+		fileman.input_websencia.click();
+	},
+	upl_folder:function(){
+		fileman.input_folder.click();	
 	},
 
 	do_delete: function (file)
@@ -227,9 +385,7 @@ var fileman={
 		if (!confirm("¿Está seguro de eliminar el archivo?")) return;
 
 		var rq={};
-		rq[file]={
-				action:"delete"
-		};
+		rq[file]={ action:"delete" };
 
 		fileman.invoque(rq, function(data)
 		{
@@ -257,20 +413,17 @@ var fileman={
 			return;
 		}
 		var rq={};
-		rq["."]={
-				action:"mkdir",
-				path:fname
-		};
+		rq["."]={ action:"mkdir", path:fname };
 
 		fileman.invoque(rq,function(data)
 		{
 			let res = Object.keys(data);
 
 			for(var i=0; i<res.length; i++){
+				
 				let r = res[i];
 
-				if(!data[r].done)
-					alert(fname + " no se ha podido crear.\n" + data[r].message);
+				if(!data[r].done) alert(fname + " no se ha podido crear.\n" + data[r].message);
 			}
 		},
 		function(message){
@@ -284,25 +437,22 @@ var fileman={
 
 		if (!nname) return;
 		if(nname.trim().length==0){
-			alert("El nombre no debe ir vacio.");
+			alert("El nombre no debe ir vacío.");
 			return;
 		}
 
 		var rq={};
-		rq[file]={
-				action:"rename",
-				as:nname
-		};
+		rq[file]={ action:"rename", as:nname };
 
 		fileman.invoque(rq,function(data)
 		{
 			let res = Object.keys(data);
 
 			for(var i=0; i<res.length; i++){
+				
 				let r = res[i];
 
-				if(!data[r].done)
-					alert(r + " no se ha podido renombrar.\n" + data[r].message);
+				if(!data[r].done) alert(r + " no se ha podido renombrar.\n" + data[r].message);
 			}
 		},
 		function(message){
@@ -319,9 +469,7 @@ var fileman={
 			return;
 		}
 		var rq={};
-		rq[fname]={
-			text:""
-		};
+		rq[fname]={ text:"" };
 
 		fileman.invoque_set_file(rq,function(data)
 		{
@@ -342,6 +490,7 @@ var fileman={
 	},
 	invoque: function (params,callback_success, callback_fail)
 	{
+		fileman.waiting();
 		$.ajax({
 			type: "POST",
 			url: fileman.folderPath+"do.fso",
@@ -372,6 +521,7 @@ var fileman={
 
 	invoqueUpFiles: function (formData,callback_success, callback_fail)
 	{
+		fileman.waiting();
 		$.ajax({
 			type: "POST",
 			url: fileman.folderPath+"upl.fso",
@@ -381,6 +531,7 @@ var fileman={
 			processData: false,  // tell jQuery not to process the data
   		
 			success: function(r){
+				
 				var res = JSON.parse(JSON.stringify(r));
 				if (res.success)
 				{
@@ -410,6 +561,7 @@ var fileman={
 			contentType: "application/json; charset=utf-8",
 			dataType: "json",
 			success: function(r){
+				
 				var res = JSON.parse(JSON.stringify(r));
 				if (res.success)
 				{
@@ -439,17 +591,30 @@ var fileman={
 			if(fileman.selectedFiles.length<1){
 				$("div.tool_bar_copy").css("pointer-events","none");
 				$("div.tool_bar_cut").css("pointer-events","none");
+				$("div.tool_bar_delete").css("pointer-events","none");
 			}else{
 				$(".tool_bar_copy").css("pointer-events","auto");
 				$(".tool_bar_cut").css("pointer-events","auto");
+				$(".tool_bar_delete").css("pointer-events","auto");
 			}
 		});
+	},
+	waiting:function(){
+		let processing = document.querySelector("#processing");
+		processing.classList.add("processing_show");
+		document.oncontextmenu = new Function("return false");
+	},
+	hidde_waiting:function(){
+		let processing = document.querySelector("#processing");
+		processing.classList.remove("processing_show");
+		document.oncontextmenu = new Function("return true");
 	}
 };
 
 function myJQueryCode() {
+
     $(document).ready(() => {
-		//
+		
 		fileman.load_drag();
         $(".privileges_select").val("");
         $("#modal_con_select").val("");
@@ -458,6 +623,7 @@ function myJQueryCode() {
 }
 
 if(typeof jQuery=='undefined') {
+
     var headTag = document.getElementsByTagName("head")[0];
     var jqTag = document.createElement('script');
     jqTag.type = 'text/javascript';
